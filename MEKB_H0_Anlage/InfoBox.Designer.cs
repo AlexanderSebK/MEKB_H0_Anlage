158a15b55052a70640fb336a61594af3ff49903c
Alexander Kühne <alexander.kuehne@onlinehome.de>
2020-11-15 22:14:08 +0100

    Info Box aktualisiert

--- MEKB_H0_Anlage/InfoBox.Designer.cs
+++ MEKB_H0_Anlage/InfoBox.Designer.cs
@@ -54,7 +54,7 @@
             this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(8, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(262, 39);
+            this.label1.Size = new System.Drawing.Size(359, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "MEKB - Steuersoftware";
             // 
@@ -64,10 +64,9 @@
             this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(8, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(303, 23);
+            this.label2.Size = new System.Drawing.Size(399, 23);
             this.label2.TabIndex = 1;
-            this.label2.Text = "H0 - Anlage mit Schattenbahnhof - Version 0.9";
-
+            this.label2.Text = "H0 - Anlage mit Schattenbahnhof - Version 1.0";
             // 
             // label3
             // 
@@ -75,7 +74,7 @@
             this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(8, 288);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(307, 17);
+            this.label3.Size = new System.Drawing.Size(422, 17);
             this.label3.TabIndex = 2;
             this.label3.Text = "Programm wurde entwickelt und gestaltet von Alexander Kühne";
             // 
@@ -86,7 +85,7 @@
             this.label4.ForeColor = System.Drawing.Color.ForestGreen;
             this.label4.Location = new System.Drawing.Point(8, 112);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(229, 17);
+            this.label4.Size = new System.Drawing.Size(311, 17);
             this.label4.TabIndex = 3;
             this.label4.Text = "Basisfkt. - Weichensteuerung (fertig - 02.09.20)";
             // 
@@ -97,7 +96,7 @@
             this.label5.ForeColor = System.Drawing.Color.ForestGreen;
             this.label5.Location = new System.Drawing.Point(8, 136);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(221, 17);
+            this.label5.Size = new System.Drawing.Size(295, 17);
             this.label5.TabIndex = 4;
             this.label5.Text = "Basisfkt. - Signalsteuerung (fertig - 25.10.20)";
             // 
@@ -108,7 +107,7 @@
             this.label6.ForeColor = System.Drawing.Color.ForestGreen;
             this.label6.Location = new System.Drawing.Point(8, 184);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(199, 17);
+            this.label6.Size = new System.Drawing.Size(266, 17);
             this.label6.TabIndex = 5;
             this.label6.Text = "Basisfkt. - Fahrstraßen (fertig - 16.09.20)";
             // 
@@ -118,7 +117,7 @@
             this.label7.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.Location = new System.Drawing.Point(8, 232);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(73, 17);
+            this.label7.Size = new System.Drawing.Size(95, 17);
             this.label7.TabIndex = 6;
             this.label7.Text = "Blockfunktion";
             // 
@@ -126,12 +125,12 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Orange;
+            this.label8.ForeColor = System.Drawing.Color.ForestGreen;
             this.label8.Location = new System.Drawing.Point(8, 160);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(176, 17);
+            this.label8.Size = new System.Drawing.Size(278, 17);
             this.label8.TabIndex = 7;
-            this.label8.Text = "Basisfkt. - Zugsteuerung  (in Arbeit)";
+            this.label8.Text = "Basisfkt. - Zugsteuerung  (fertig - 15.11.20)";
             // 
             // label9
             // 
@@ -140,7 +139,7 @@
             this.label9.ForeColor = System.Drawing.Color.ForestGreen;
             this.label9.Location = new System.Drawing.Point(384, 112);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(208, 17);
+            this.label9.Size = new System.Drawing.Size(286, 17);
             this.label9.TabIndex = 8;
             this.label9.Text = "alle Weichen zugeordnet  (fertig - 02.09.20)";
             // 
@@ -151,7 +150,7 @@
             this.label10.ForeColor = System.Drawing.Color.ForestGreen;
             this.label10.Location = new System.Drawing.Point(384, 136);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(205, 17);
+            this.label10.Size = new System.Drawing.Size(278, 17);
             this.label10.TabIndex = 9;
             this.label10.Text = "alle Signale zugeordnet  (fertig - 24.10.20)";
             // 
@@ -162,7 +161,7 @@
             this.label11.ForeColor = System.Drawing.Color.Orange;
             this.label11.Location = new System.Drawing.Point(384, 184);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(189, 17);
+            this.label11.Size = new System.Drawing.Size(261, 17);
             this.label11.TabIndex = 10;
             this.label11.Text = "alle Fahrstraßen zugeordnet (in Arbeit)";
             // 
@@ -172,7 +171,7 @@
             this.label12.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(8, 208);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(121, 17);
+            this.label12.Size = new System.Drawing.Size(164, 17);
             this.label12.TabIndex = 11;
             this.label12.Text = "Basisfkt. - Belegtmelder";
             // 
@@ -183,7 +182,7 @@
             this.label13.ForeColor = System.Drawing.Color.Black;
             this.label13.Location = new System.Drawing.Point(384, 208);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(116, 17);
+            this.label13.Size = new System.Drawing.Size(160, 17);
             this.label13.TabIndex = 12;
             this.label13.Text = "alle Blöcke zugeordnet ";
             // 
@@ -193,7 +192,7 @@
             this.label14.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.Location = new System.Drawing.Point(384, 232);
             this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(136, 17);
+            this.label14.Size = new System.Drawing.Size(188, 17);
             this.label14.TabIndex = 13;
             this.label14.Text = "Bremsweg / Zwangsbremse";
             // 
@@ -203,7 +202,7 @@
             this.label15.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label15.Location = new System.Drawing.Point(8, 256);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(117, 17);
+            this.label15.Size = new System.Drawing.Size(161, 17);
             this.label15.TabIndex = 14;
             this.label15.Text = "Autom. Fahrdienstleiter";
             // 
@@ -213,7 +212,7 @@
             this.label16.Font = new System.Drawing.Font("Bahnschrift Condensed", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label16.Location = new System.Drawing.Point(384, 256);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(107, 17);
+            this.label16.Size = new System.Drawing.Size(147, 17);
             this.label16.TabIndex = 15;
             this.label16.Text = "Autom. Zugsteuerung";
             // 
