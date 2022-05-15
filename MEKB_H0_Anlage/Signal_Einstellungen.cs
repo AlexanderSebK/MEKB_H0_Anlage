c60f2fc41132f7210e3a50fbff8700468c5f67b1
Alexander Kühne <alexander.kuehne@onlinehome.de>
2022-05-15 18:25:16 +0200

    Kommentare bei Signaleinstellungen hinzugefügt

--- MEKB_H0_Anlage/Signal_Einstellungen.cs
+++ MEKB_H0_Anlage/Signal_Einstellungen.cs
@@ -12,16 +12,31 @@ namespace MEKB_H0_Anlage
 {
     public partial class Signal_Einstellungen : Form
     {
+        /// <summary>
+        /// Constructor - Initialisieren des Fensters
+        /// </summary>
         public Signal_Einstellungen()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Button:
+        /// Fenster schließen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void SignalEinstellungenSchliesen_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
+        /// <summary>
+        /// Event: Aufbau des Fensters
+        /// Lesen der Konfiguration und setzen der Check-Boxen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Signal_Einstellungen_Shown(object sender, EventArgs e)
         {
             if(Config.ReadConfig("AutoSignalFahrstrasse").Equals("true"))
@@ -44,16 +59,31 @@ namespace MEKB_H0_Anlage
             }
         }
 
+        /// <summary>
+        /// Einstellung ändern: Steuerung über Stellung der Weichen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AutoSigStellung_CheckedChanged(object sender, EventArgs e)
         {
             if(AutoSigStellung.Checked) Config.WriteConfig("AutoSignalFahrstrasse", "false");
         }
 
+        /// <summary>
+        /// Einstellung ändern: Steuerung über Sicherheitsstatus der Fahrstraße
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AutoSigStrasse_CheckedChanged(object sender, EventArgs e)
         {
             if (AutoSigStrasse.Checked) Config.WriteConfig("AutoSignalFahrstrasse", "true");
         }
 
+        /// <summary>
+        /// Einstellung ändern: Erlaube/Verbiete automatisches Schalten auf Fahrt
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AutoFahrt_CheckedChanged(object sender, EventArgs e)
         {
             if(AutoFahrt.Checked) Config.WriteConfig("AutoSignalFahrt", "true");
